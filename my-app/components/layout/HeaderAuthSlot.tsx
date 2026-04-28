287a1877da922e4463f937269f3a5a68af88e57e
mingi <jmgi1024@gmail.com>
2026-04-28 12:22:43 +0900

    Refactor mobile header navigation

--- my-app/components/layout/HeaderAuthSlot.tsx
+++ my-app/components/layout/HeaderAuthSlot.tsx
@@ -1,7 +1,10 @@
 "use client";
 
+import Link from "next/link";
+import type { MouseEventHandler } from "react";
 import { useClerkAvailable } from "../providers/ClerkAvailabilityProvider";
-import { AuthLinks, ClerkAuthButtons } from "./ClerkAuthButtons";
+import { AuthLinks, ClerkAuthButtons, MobileClerkAuthButtons, MobileClerkSignupMenuLink } from "./ClerkAuthButtons";
+import { loginButtonClassName } from "./authButtonStyles";
 
 export function HeaderAuthSlot() {
   const hasClerkProvider = useClerkAvailable();
@@ -11,4 +14,37 @@ export function HeaderAuthSlot() {
   }
 
   return <ClerkAuthButtons />;
+}
+
+export function MobileHeaderAuthSlot() {
+  const hasClerkProvider = useClerkAvailable();
+
+  if (!hasClerkProvider) {
+    return (
+      <Link href="/login" className={loginButtonClassName}>
+        로그인
+      </Link>
+    );
+  }
+
+  return <MobileClerkAuthButtons />;
+}
+
+interface MobileSignupMenuLinkProps {
+  className: string;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+}
+
+export function MobileSignupMenuLink({ className, onClick }: MobileSignupMenuLinkProps) {
+  const hasClerkProvider = useClerkAvailable();
+
+  if (!hasClerkProvider) {
+    return (
+      <Link href="/signup" onClick={onClick} className={className}>
+        회원가입
+      </Link>
+    );
+  }
+
+  return <MobileClerkSignupMenuLink className={className} onClick={onClick} />;
 }
